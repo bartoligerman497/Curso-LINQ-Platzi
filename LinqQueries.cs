5e70157cd71f91a1296f5799b4b33d7a32b5583b
German Bartoli <bartoligerman497@gmail.com>
2022-11-30 23:42:59 -0300

    Update LinqQueries.cs
    Acomodando

--- LinqQueries.cs
+++ LinqQueries.cs
@@ -7,7 +7,8 @@ public class LinqQueries
         using(StreamReader reader = new StreamReader("books.json"))
         {
             string json = reader.ReadToEnd();
-            this.librosCollection = System.Text.Json.JsonSerializer.Deserialize<List<Book>>(json, new System.Text.Json.JsonSerializerOptions() {PropertyNameCaseInsensitive = true});
+            this.librosCollection = 
+            System.Text.Json.JsonSerializer.Deserialize<List<Book>>(json, new System.Text.Json.JsonSerializerOptions() {PropertyNameCaseInsensitive = true});
         }
     }
 
@@ -19,45 +20,60 @@ public class LinqQueries
     public IEnumerable<Book> LibrosDespuesdel2000()
     {
         //extension method
-        //return librosCollection.Where(p=> p.PublishedDate.Year > 2000);
+        //return librosCollection.
+        //Where(p=> p.PublishedDate.Year > 2000);
 
         //query expresion
 
-        return from l in librosCollection where l.PublishedDate.Year > 2000 select l;
+        return from 
+                l in librosCollection 
+                where l.PublishedDate.Year > 2000 
+                select l;
     }
 
     public IEnumerable<Book> LibrosConMasde250PagConPalabrasInAction()
     {
         //extension methods
-        //return librosCollection.Where(p=> p.PageCount > 250 && p.Title.Contains("in Action"));
+        //return librosCollection
+        //.Where(p=> p.PageCount > 250 && p.Title.Contains("in Action"));
 
         //query expression
-        return from l in librosCollection where l.PageCount > 250 && l.Title.Contains("in Action") select l;
+        return from 
+                    l in librosCollection 
+                    where l.PageCount > 250 && l.Title.Contains("in Action") 
+                    select l;
     }
 
     public bool TodosLosLibrosTienenStatus()
     {
-        return librosCollection.All(p=> p.Status!= string.Empty);
+        return librosCollection
+        .All(p=> p.Status!= string.Empty);
     }
 
     public bool SiAlgunLibroFuePublicado2005()
     {
-        return librosCollection.Any(p=> p.PublishedDate.Year == 2005);
+        return librosCollection
+        .Any(p=> p.PublishedDate.Year == 2005);
     }
 
     public IEnumerable<Book> LibrosdePython()
     {
-        return librosCollection.Where(p=> p.Categories.Contains("Python"));
+        return librosCollection
+        .Where(p=> p.Categories.Contains("Python"));
     }
 
     public IEnumerable<Book> LibrosdeJavaPorNombreAscendente()
     {
-        return librosCollection.Where(p=> p.Categories.Contains("Java")).OrderBy(p=> p.Title);
+        return librosCollection
+        .Where(p=> p.Categories.Contains("Java"))
+        .OrderBy(p=> p.Title);
     }
 
     public IEnumerable<Book> Librosmasde450pagOrdernadorPorNumPagDescendente()
     {
-        return librosCollection.Where(p=> p.PageCount > 450).OrderByDescending(p=> p.PageCount);
+        return librosCollection
+        .Where(p=> p.PageCount > 450)
+        .OrderByDescending(p=> p.PageCount);
     }
 
     public IEnumerable<Book> TresPrimerosLibrosJavaOrdenadosPorFecha()
@@ -83,13 +99,20 @@ public class LinqQueries
 
     public IEnumerable<Book> TresPrimeroLibrosDeLaCollecion()
     {
-        return librosCollection.Take(3)
+        return librosCollection
+        .Take(3)
         .Select(p=> new Book() { Title= p.Title, PageCount= p.PageCount  });
     }
 
-    public long CantidadDeLibrosEntre200y500Pag()
+    public int CantidadDeLibrosEntre200y500Pag()
     {
-        return librosCollection.LongCount(p=> p.PageCount>=200 && p.PageCount<=500);
+        return librosCollection.Where(p=> p.PageCount>=200 && p.PageCount<=500).Count() ;
+
+        
+        // return librosCollection
+        // .LongCount(p=> p.PageCount>=200 && p.PageCount<=500);
+
+
     }
 
     public DateTime FechaDePublicacionMenor()
